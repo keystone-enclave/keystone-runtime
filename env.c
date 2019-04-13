0655b52057c78194cb831912c831057a58d19206
dkohlbre <dkohlbre@cs.ucsd.edu>
2019-04-12 17:27:22 -0700

    Added uname support, fixed missing rt utils

--- env.c
+++ env.c
@@ -1,6 +1,7 @@
 #include "elf.h"
 #include "tmplib/uaccess.h"
 #include "rt_util.h"
+#include "string.h"
 /******
  *
  * This is for setting up a minimal ENV/AUX etc to let libc init
@@ -24,9 +25,8 @@
 // How many AUX things are we actually defining? Add one for terminator
 #define AUXV_COUNT 11
 
-
-// Size in number-of-words (argc, argv, null_env, auxv, randombytes)
-#define SIZE_OF_SETUP (1+1+1+(2*AUXV_COUNT) + 16)
+// Size in number-of-words (argc, argv, null_env, auxv, randombytes
+#define SIZE_OF_SETUP (1+1+1+(2*AUXV_COUNT) + 2)
 
 // We return the new sp
 void* setup_start(void* _sp){
@@ -38,10 +38,11 @@ void* setup_start(void* _sp){
   sp = sp - SIZE_OF_SETUP;
 
   //setup argc
-  staging[0] = 0;
+  staging[0] = (void*)0;
 
   //argv
   staging[1] = (void*)0; // NULL
+  //TODO We need to support /at least/ argv[0]
 
   //envp[0] (terminate)
   staging[2] = (void*)0; // NULL
@@ -80,12 +81,12 @@ void* setup_start(void* _sp){
   // should be that i == AUXV_COUNT*2
 
   // Ask for random values
-   int ret = rt_util_getrandom((void*)&auxv[i], 16);
+  int ret = rt_util_getrandom((void*)&auxv[i], 16);
   if( ret != 0 )
   rt_util_misc_fatal();
   //Assign the ptr
   *at_random_ptr = &sp[i+3];
-
+  i+=2; // fixup i
 
   // We don't do PHDR or PHNUM etc right now, TLS might need it though
 
