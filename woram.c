645f9bcba31683b31c39ae39ac5d6055d6bc1d8b
supriya363 <supriya.s363@gmail.com>
2020-05-01 18:24:28 +0530

    modified sanity check

--- woram.c
+++ woram.c
@@ -6,11 +6,33 @@ void initialize_woram_array()
     // uintptr_t utm_size = shared_buffer_size;
 
     woram_array = utm_start + UTM_ARRAY_STARTING_OFFSET;
+    sanity_check();
+    
+}
 
+void sanity_check()
+{
     //sanity check - access 1st 1MB of the oram array
     char *ptr = (char*) woram_array;
     for( int i=0; i<1024*1024; i++)
         ptr[i] = i;
+
+    //sanity check - access 1st 1000 pages of the oram array
+    pages *pages_ptr = (pages*) woram_array;
+    // printf("size of 1 page struct is 0x%zx\n", sizeof(pages)); //4152
+    for(int i=0; i<1000; i++)
+        pages_ptr[i].address = woram_array;
+
+    if (pages_ptr[9].address != woram_array)
+    {
+        printf("[runtime] woram sanity check failed. Exiting\n");
+        sbi_exit_enclave(-1);
+    }
+}
+
+void initialize_position_map(void)
+{
+    //TODO - initialize position_map using malloc
 }
 
 
--- woram.h
+++ woram.h
@@ -4,12 +4,15 @@
 #include "vm.h"
 
 #define UTM_ARRAY_STARTING_OFFSET 1048*1048 //1MB
+#define POSITION_MAP_SIZE 1024
 
 uintptr_t woram_array;
 uintptr_t woram_array_size;
-
+uintptr_t position_map;
 
 
 void initialize_woram_array(void);
+void initialize_position_map(void);
+void sanity_check(void);
 
 #endif
