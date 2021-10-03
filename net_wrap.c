1b96a0c38e2436196275802722808c872253aef2
Catherine Lu <cathylu@berkeley.edu>
2021-10-03 04:04:58 +0000

    Debug

--- net_wrap.c
+++ net_wrap.c
@@ -154,7 +154,7 @@ uintptr_t io_syscall_getsockname(int sockfd, uintptr_t addr,
   edge_syscall->syscall_num = SYS_getsockname;
   args->sockfd = sockfd;
   print_strace("[runtime] in getsockname: fd: %d\r\n", args->sockfd);
-  print_strace("[runtime] addrlen, addr: %d", * (socklen_t*) addrlen);
+  print_strace("[runtime] addrlen, addr: %d", addrlen);
 
   if(addrlen > sizeof(struct sockaddr_storage)) {
     print_strace("[runtime] proxied getsockname: fd: %d, ret: %d\r\n", args->sockfd, ret);
